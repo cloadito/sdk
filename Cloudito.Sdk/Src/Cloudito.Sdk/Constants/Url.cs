b1baa6f33197484af9459cb358bd66b84f47d944
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-05-02 23:09:19 +0330

    update : add urls

--- Cloudito.Sdk/Src/Cloudito.Sdk/Constants/Url.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Constants/Url.cs
@@ -2,12 +2,23 @@
 
 internal class UrlsConst
 {
+    public const string V1 = "v1.0";
+
+    public const string V1BaseUrl = $"/api/{V1}";
+
     public class Identity
     {
-        public const string _BaseUrl = "/api/v1.0/";
+        public const string SendOtp = $"{V1BaseUrl}auth/send-otp";
 
-        public const string SendOtp = $"{_BaseUrl}auth/send-otp";
+        public const string LoginOtp = $"{V1BaseUrl}auth/login-otp";
+    }
 
-        public const string LoginOtp = $"{_BaseUrl}auth/login-otp";
+    public class Wallet
+    {
+        public const string Init = $"{V1BaseUrl}/wallet/init";
+
+        public const string GetUserWallet = $"{V1BaseUrl}/wallet/get-user-wallet";
+
+        public const string GetInventory = $"{V1BaseUrl}/wallet/get-inventory";
     }
 }
