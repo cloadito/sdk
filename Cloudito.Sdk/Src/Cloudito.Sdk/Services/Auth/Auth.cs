334008318709c0a24e55d5a02d51c84419d89868
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-05-02 23:04:22 +0330

    create : auth services

---
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Auth/Auth.cs
@@ -0,0 +1,37 @@
+﻿namespace Cloudito.Sdk.Services;
+
+internal class Auth(IRest rest) : IAuth
+{
+    public async Task<ServiceResult<LoginResult>> LoginOtpAsync(string userName, string code)
+    {
+        try
+        {
+            ApiModel<LoginResult> request = await rest.PostAsync<LoginResult>(Constants.UrlsConst.LoginOtp, new
+            {
+                UserName = userName,
+                Code = code
+            });
+            return ServiceResult<LoginResult>.FromApi(request);
+        }
+        catch (Exception ex)
+        {
+            return ServiceResult<LoginResult>.Error(ex.Message);
+        }
+    }
+
+    public async Task<ServiceResult<object>> SendOtpAsync(string userName)
+    {
+        try
+        {
+            ApiModel<object> request = await rest.PostAsync<object>(Constants.UrlsConst.SendOtp, new
+            {
+                UserName = userName
+            });
+            return ServiceResult<object>.FromApi(request);
+        }
+        catch (Exception ex)
+        {
+            return ServiceResult<object>.Error(ex.Message);
+        }
+    }
+}
---
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Auth/IAuth.cs
@@ -0,0 +1,8 @@
+﻿namespace Cloudito.Sdk.Services;
+
+public interface IAuth
+{
+    Task<ServiceResult<object>> SendOtpAsync(string userName);
+
+    Task<ServiceResult<LoginResult>> LoginOtpAsync(string userName, string code);
+}
