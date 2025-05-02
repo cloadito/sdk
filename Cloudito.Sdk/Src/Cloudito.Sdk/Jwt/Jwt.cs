660c78accd7923eaad2dca5d872d5360df5ba87c
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-05-02 23:03:38 +0330

    Create Jwt.cs

---
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Jwt/Jwt.cs
@@ -0,0 +1,51 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace Cloudito.Sdk;
+
+internal class JwtTokenGenerator
+{
+    public static User? GetUserData(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        try
+        {
+            // Parse the token
+            var jwtToken = tokenHandler.ReadJwtToken(token);
+
+            // Extract the user ID (assumes it's stored in the "sub" claim)
+            var id = jwtToken.Claims.FirstOrDefault(c => c.Type == Constants.JwtConstnats.Id)?.Value;
+            var name = jwtToken.Claims.FirstOrDefault(c => c.Type == Constants.JwtConstnats.FirstName)?.Value;
+            var lastName = jwtToken.Claims.FirstOrDefault(c => c.Type == Constants.JwtConstnats.LastName)?.Value;
+            var mobileNo = jwtToken.Claims.FirstOrDefault(c => c.Type == Constants.JwtConstnats.MobileNo)?.Value;
+            var email = jwtToken.Claims.FirstOrDefault(c => c.Type == Constants.JwtConstnats.Email)?.Value;
+
+            if (id is null)
+                return null;
+
+            _ = Guid.TryParse(id, out Guid userId);
+            return new(userId, name ?? "", lastName ?? "", mobileNo ?? "", email ?? "");
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public static bool IsTokenExpired(string token)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        try
+        {
+            var jwtToken = tokenHandler.ReadJwtToken(token);
+            var expirationDate = jwtToken.ValidTo; // Extract expiration time (UTC)
+
+            return expirationDate < DateTime.Now; // Return true if expired
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
