f7e19f2dcfee4c33c427b7d4fda4342c3c41e850
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-05-08 15:55:46 +0330

    create : ticket test

---
+++ Cloudito.Sdk/Test/Cloudito.Sdk.Test/Ticket/Ticket.cs
@@ -0,0 +1,11 @@
+﻿using Cloudito.Sdk.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cloudito.Sdk.Test;
+
+internal class TicketTest(TestFixture fixture, ITestOutputHelper outputHelper) : IClassFixture<TestFixture>
+{
+    private readonly ITicket _ticket = fixture.ServiceProvider.GetRequiredService<ITicket>();
+}
