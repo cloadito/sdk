6ffa4f7806218f297efac73058171bc04e30a408
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-05-08 16:22:44 +0330

    Update Message.cs

--- Cloudito.Sdk/Src/Cloudito.Sdk/Model/Ticket/Message.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Model/Ticket/Message.cs
@@ -1,10 +1,5 @@
 ﻿namespace Cloudito.Sdk;
 
-/// <summary>
-/// Ticket message model
-/// </summary>
-/// <param name="Id">nullable id that use for upsert</param>
-/// <param name="ReplyId">reply message id</param>
-/// <param name="SenderId">sender message id</param>
-/// <param name="Text">message text</param>
-public record TicketMessage(Guid? Id, Guid? ReplyId, Guid SenderId, string Text);
+public record TicketMessage(Guid Id, Guid? ReplyId, Guid SenderId, string Text);
+
+public record UpsertTicketMessage(Guid TicketId, Guid? Id, Guid? ReplyId, Guid SenderId, string Text);
