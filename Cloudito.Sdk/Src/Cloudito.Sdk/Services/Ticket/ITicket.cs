6b8248432b66c958d696f356e4327d64bb1d0d26
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-05-08 15:41:14 +0330

    create : ticket services

---
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Ticket/ITicket.cs
@@ -0,0 +1,12 @@
+﻿namespace Cloudito.Sdk.Services;
+
+public interface ITicket
+{
+    /// <summary>
+    /// Get list of departments for application
+    /// </summary>
+    /// <returns>List of <see cref="Department"/></returns>
+    Task<ServiceResult<IEnumerable<Department>>> GetDepartmentsAsync();
+
+    Task<ServiceResult<Pagination<>>>
+}
---
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Ticket/Ticket.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Cloudito.Sdk.Services;
+
+internal class TicketService(IBaseService baseService) : ITicket
+{
+    public async Task<ServiceResult<IEnumerable<Department>>> GetDepartmentsAsync()
+        => await baseService.CallServiceAsync<IEnumerable<Department>>(UrlsConst.Ticket.GetDepartmetns, null, HttpMethod.Get);
+}
