09bf2f76f7609237f601ca907ffcbd62bbb7f67b
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-05-08 16:22:40 +0330

    create : test constants

---
+++ Cloudito.Sdk/Test/Cloudito.Sdk.Test/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Cloudito.Sdk;
+
+internal class Constants
+{
+    public static Guid userId = Guid.Parse("bb0945d6-8172-4cd4-9db7-d80376bd9711");
+    public static Guid walletId = Guid.Parse("a90d5b73-b516-4901-8cf1-a999db468932");
+    public static Guid ticketId = Guid.Parse("f9d0ce03-c7e1-4034-8e86-d6fd1cfdebbf");
+}
