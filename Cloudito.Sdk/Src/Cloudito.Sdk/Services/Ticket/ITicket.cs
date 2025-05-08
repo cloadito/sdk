6b5de45fa1a0eddd13e6158015faa0ceacdb778d
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-05-08 15:47:02 +0330

    update : add service for get user tickets

--- Cloudito.Sdk/Src/Cloudito.Sdk/Services/Ticket/ITicket.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Ticket/ITicket.cs
@@ -16,4 +16,14 @@ public interface ITicket
     /// <param name="count">page size</param>
     /// <returns>Pagination model of <see cref="Ticket"/></returns>
     Task<ServiceResult<Pagination<Ticket>>> GetAppTicketsAsync(Guid? departmentId, int page, int count);
+
+    /// <summary>
+    /// Get user tickets list
+    /// </summary>
+    /// <param name="userId">User id that wanna get tickets</param>
+    /// <param name="departmentId">filter by department id</param>
+    /// <param name="page">page index that zero index</param>
+    /// <param name="count">page size</param>
+    /// <returns>Pagination model of <see cref="Ticket"/></returns>
+    Task<ServiceResult<Pagination<Ticket>>> GetUserTicketsAsync(Guid userId, Guid? departmentId, int page, int count);
 }
--- Cloudito.Sdk/Src/Cloudito.Sdk/Services/Ticket/Ticket.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Ticket/Ticket.cs
@@ -9,4 +9,7 @@ internal class TicketService(IBaseService baseService) : ITicket
 
     public async Task<ServiceResult<IEnumerable<Department>>> GetDepartmentsAsync()
         => await baseService.CallServiceAsync<IEnumerable<Department>>(UrlsConst.Ticket.GetDepartmetns, null, HttpMethod.Get);
+
+    public async Task<ServiceResult<Pagination<Ticket>>> GetUserTicketsAsync(Guid userId, Guid? departmentId, int page, int count)
+        => await baseService.CallServiceAsync<Pagination<Ticket>>(UrlsConst.Ticket.GetUserTickets(userId, departmentId, page, count), null, HttpMethod.Get);
 }
