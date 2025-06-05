ee881e0f76c024a2c08682642e7243ef11742a0a
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-06-05 23:23:01 +0330

    create : user services

---
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Identity/Abstraction/IUser.cs
@@ -0,0 +1,16 @@
+﻿namespace Cloudito.Sdk.Services;
+
+public interface IUser
+{
+    Task<ServiceResult<User?>> FindByIdAsync(Guid id);
+
+    Task<ServiceResult<User?>> ForceRegisterAsync(ForceRegisterRequest request);
+
+    Task<ServiceResult<Pagination<User>>> GetUsersAsync(int page, int count);
+
+    Task<ServiceResult<IEnumerable<Role>>> GetUserRolesAsync(Guid userId);
+
+    Task<ServiceResult<bool>> IsInRoleAsync(Guid userId, string role);
+
+    Task<ServiceResult<bool>> HasAccessAsync(Guid userId, string page, string action);
+}
---
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Identity/Implementation/UserService.cs
@@ -0,0 +1,27 @@
+﻿namespace Cloudito.Sdk.Services;
+
+internal class UserService(IBaseService baseService) : IUser
+{
+    public async Task<ServiceResult<User?>> FindByIdAsync(Guid id)
+        => await baseService.CallServiceAsync<User?>(UrlsConst.Identity.FindUserById(id), null, HttpMethod.Get);
+
+    public async Task<ServiceResult<User?>> ForceRegisterAsync(ForceRegisterRequest request)
+        => await baseService.CallServiceAsync<User?>(UrlsConst.Identity.ForceRegister, request, HttpMethod.Post);
+
+    public async Task<ServiceResult<IEnumerable<Role>>> GetUserRolesAsync(Guid userId)
+        => await baseService.CallServiceAsync<IEnumerable<Role>>(UrlsConst.Identity.GetUserRoles(userId), null, HttpMethod.Get);
+
+    public async Task<ServiceResult<Pagination<User>>> GetUsersAsync(int page, int count)
+        => await baseService.CallServiceAsync<Pagination<User>>(UrlsConst.Identity.GetAppUsers(page, count), null, HttpMethod.Get);
+
+    public async Task<ServiceResult<bool>> HasAccessAsync(Guid userId, string page, string action)
+        => await baseService.CallServiceAsync<bool>(UrlsConst.Identity.HasAccess, new
+        {
+            UserId = userId,
+            Page = page,
+            Action = action
+        }, HttpMethod.Post);
+
+    public async Task<ServiceResult<bool>> IsInRoleAsync(Guid userId, string role)
+        => await baseService.CallServiceAsync<bool>(UrlsConst.Identity.IsInRole(userId, role), null, HttpMethod.Get);
+}
