933a39cb30594e39ae0884e82cff38269e384b27
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-06-05 23:22:41 +0330

    Update User.cs

--- Cloudito.Sdk/Src/Cloudito.Sdk/Model/Identity/User/User.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Model/Identity/User/User.cs
@@ -3,3 +3,5 @@
 public record User(Guid Id, string FirstName, string LastName, string MobileNo, string Email);
 
 public record SetPassword(string UserName, string Code, string NewPassword, string ReNewPassword);
+
+public record ForceRegisterRequest(string FirstName, string LastName, string MobileNo, string Email);
