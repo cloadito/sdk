6b583506ff0115ca9d39628085bac90b15da5450
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-06-11 22:59:08 +0330

    update : add new urls

--- Cloudito.Sdk/Src/Cloudito.Sdk/Constants/Url.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Constants/Url.cs
@@ -63,6 +63,10 @@ internal class UrlsConst
         public const string IsAdmin = $"{V1BaseUrl}/application/is-admin";
 
         public const string MakeTransaction = $"{V1BaseUrl}/application/make-transaction";
+
+        public const string MakeBulkTransaction = $"{V1BaseUrl}/application/make-bulk-transaction";
+
+        public const string LogUsage = $"{V1BaseUrl}/application/log-usage";
     }
 
     public class Ticket
