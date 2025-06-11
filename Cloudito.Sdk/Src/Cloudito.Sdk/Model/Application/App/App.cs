50dfb14dcdc72e901ef9b184597a76dbb3bcd38f
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-06-11 23:02:52 +0330

    update : create new models
    - model for transaction
    - model for log

--- Cloudito.Sdk/Src/Cloudito.Sdk/Model/Application/App/App.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Model/Application/App/App.cs
@@ -1,3 +1,7 @@
 ﻿namespace Cloudito.Sdk;
 
 public record Application(Guid Id, string Name);
+
+public record MakeTransaction(Guid AppId, decimal Amount);
+
+public record LogUsage(Guid? AppId, string Endpoint, DateTime Datetime);
