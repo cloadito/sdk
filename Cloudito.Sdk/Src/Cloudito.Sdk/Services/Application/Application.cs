897c3f7593b2792255d8d7c6aa00118181e86ff4
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-06-11 23:03:03 +0330

    update : implement new services

--- Cloudito.Sdk/Src/Cloudito.Sdk/Services/Application/Application.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Application/Application.cs
@@ -1,4 +1,5 @@
-﻿namespace Cloudito.Sdk.Services;
+﻿
+namespace Cloudito.Sdk.Services;
 
 internal class ApplicationService(IBaseService baseService) : IApplication
 {
@@ -9,10 +10,18 @@ internal class ApplicationService(IBaseService baseService) : IApplication
             AppId = appId,
         }, HttpMethod.Post);
 
-    public async Task<ServiceResult<object>> MakeTransactionAsync(Guid appId, decimal amount)
-         => await baseService.CallServiceAsync<object>(UrlsConst.Applicaitons.MakeTransaction, new
-         {
-             AppId = appId,
-             Amount = amount,
-         }, HttpMethod.Post);
+    public async Task<ServiceResult<object>> LogUsageAsync(IEnumerable<LogUsage> logs)
+        => await baseService.CallServiceAsync<object>(UrlsConst.Applicaitons.LogUsage, new
+        {
+            Logs = logs
+        }, HttpMethod.Post);
+
+    public async Task<ServiceResult<object>> MakeBulkTransactionAsync(IEnumerable<MakeTransaction> transactions)
+        => await baseService.CallServiceAsync<object>(UrlsConst.Applicaitons.MakeBulkTransaction, new
+        {
+            transactions = transactions,
+        }, HttpMethod.Post);
+
+    public async Task<ServiceResult<object>> MakeTransactionAsync(MakeTransaction transaction)
+         => await baseService.CallServiceAsync<object>(UrlsConst.Applicaitons.MakeTransaction, transaction, HttpMethod.Post);
 }
--- Cloudito.Sdk/Src/Cloudito.Sdk/Services/Application/IApplication.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Application/IApplication.cs
@@ -5,4 +5,10 @@ public interface IApplication
     Task<ServiceResult<bool>> IsAdminAsync(Guid userId, Guid appId);
 
     Task<ServiceResult<object>> MakeTransactionAsync(Guid appId, decimal amount);
+
+    Task<ServiceResult<object>> MakeTransactionAsync(MakeTransaction transaction);
+
+    Task<ServiceResult<object>> MakeBulkTransactionAsync(IEnumerable<MakeTransaction> transactions);
+
+    Task<ServiceResult<object>> LogUsageAsync(IEnumerable<LogUsage> logs);
 }
