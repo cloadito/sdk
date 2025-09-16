1709f16d8f090925b48af903dc93c863bf6d8a27
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-09-16 23:09:46 +0330

    update : implement service

--- Cloudito.Sdk/Src/Cloudito.Sdk/Services/Notification/Abstraction/INotifGroup.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Notification/Abstraction/INotifGroup.cs
@@ -2,5 +2,15 @@ namespace Cloudito.Sdk.Services;
 
 public interface INotifGroup
 {
-    
+    Task<ServiceResult<Pagination<NotifGroup>>> GetGroupsAsync(int page, int count,
+        CancellationToken cancellationToken = default);
+
+    Task<ServiceResult<bool>> IsMemberAsync(Guid groupId, string memberId,
+        CancellationToken cancellationToken = default);
+
+    Task<ServiceResult<object>> AddMemberAsync(UpdateMemberNotifGroupRequest request,
+        CancellationToken cancellationToken = default);
+
+    Task<ServiceResult<object>> RemoveMemberAsync(UpdateMemberNotifGroupRequest request,
+        CancellationToken cancellationToken = default);
 }
--- Cloudito.Sdk/Src/Cloudito.Sdk/Services/Notification/Implementation/NotifGroupService.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Services/Notification/Implementation/NotifGroupService.cs
@@ -1,6 +1,22 @@
 namespace Cloudito.Sdk.Services;
 
-public class NotifGroupService : INotifGroup
+public class NotifGroupService(IBaseService baseService) : INotifGroup
 {
-    
+    public Task<ServiceResult<Pagination<NotifGroup>>> GetGroupsAsync(int page, int count,
+        CancellationToken cancellationToken = default)
+        => baseService.CallServiceAsync<Pagination<NotifGroup>>(UrlsConst.Notif.GetGroups(page, count), null,
+            HttpMethod.Get, cancellationToken);
+
+    public Task<ServiceResult<bool>> IsMemberAsync(Guid groupId, string memberId,
+        CancellationToken cancellationToken = default)
+        => baseService.CallServiceAsync<bool>(UrlsConst.Notif.IsMember(groupId, memberId), null, HttpMethod.Get,
+            cancellationToken);
+
+    public Task<ServiceResult<object>> AddMemberAsync(UpdateMemberNotifGroupRequest request,
+        CancellationToken cancellationToken = default)
+        => baseService.CallServiceAsync(UrlsConst.Notif.AddMember, request, HttpMethod.Post, cancellationToken);
+
+    public Task<ServiceResult<object>> RemoveMemberAsync(UpdateMemberNotifGroupRequest request,
+        CancellationToken cancellationToken = default)
+        => baseService.CallServiceAsync(UrlsConst.Notif.RemoveMember, request, HttpMethod.Post, cancellationToken);
 }
