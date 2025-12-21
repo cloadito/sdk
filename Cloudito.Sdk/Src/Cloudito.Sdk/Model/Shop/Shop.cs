2cb5a2fbaa3526eb12fd6305402bb9534c721868
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-12-21 14:57:19 +0330

    update

--- Cloudito.Sdk/Src/Cloudito.Sdk/Model/Shop/Shop.cs
+++ Cloudito.Sdk/Src/Cloudito.Sdk/Model/Shop/Shop.cs
@@ -4,7 +4,7 @@ public record Shop(Guid Id, Guid AppId, string Name, ShopStatus Status);
 
 public record ShopStatus(Guid Id, Guid AppId, string Name, string Title, string Code);
 
-public record UpsertShop(Guid? Id, string Name, Guid StatusId, Guid TypeId);
+public record UpsertShop(Guid? Id, string Name, Guid OwnerId, Guid StatusId, Guid TypeId);
 
 public record ShopType(Guid Id, Guid AppId, string Name, string Title, string Code);
 
